e2ee4f32c010ea27e55d0f307280f76913fe8a24
Kevin <diasks2@gmail.com>
2013-05-26 08:35:14 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonics::Application.config.secret_token = '5cd29f68a8e7e9728d380663b3512fbfd00543f3f6f60f9cdd8fa9c70efca64224e1b8dd31676d14d67ad9a8b6be3f39af24607a2f56cf2338138b0cd8c07adf'
