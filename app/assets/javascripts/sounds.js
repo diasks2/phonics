d9cf41a582d0a703618d3ec74fc99e4dbae5f6c5
Kevin <diasks2@gmail.com>
2013-05-26 15:24:21 +0900

    Add sounds for all letters

---
+++ app/assets/javascripts/sounds.js
@@ -0,0 +1,340 @@
+$(function() {
+	$('#letterA').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_1").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Aa_LP.mp3",
+					oga: "/assets/Aa_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterB').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_2").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Bb_LP.mp3",
+					oga: "/assets/Bb_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});
+	$('#letterC').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_3").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Cc_LP.mp3",
+					oga: "/assets/Cc_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterD').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_4").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Dd_LP.mp3",
+					oga: "/assets/Dd_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterE').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_5").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Ee_LP.mp3",
+					oga: "/assets/Ee_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterF').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_6").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Ff_LP.mp3",
+					oga: "/assets/Ff_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterG').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_7").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Gg_LP.mp3",
+					oga: "/assets/Gg_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterH').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_8").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Hh_LP.mp3",
+					oga: "/assets/Hh_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterI').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_9").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Ii_LP.mp3",
+					oga: "/assets/Ii_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterJ').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_10").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Jj_LP.mp3",
+					oga: "/assets/Jj_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterK').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_11").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Kk_LP.mp3",
+					oga: "/assets/Kk_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterL').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_12").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Ll_LP.mp3",
+					oga: "/assets/Ll_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterM').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_13").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Mm_LP.mp3",
+					oga: "/assets/Mm_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterN').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_14").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Nn_LP.mp3",
+					oga: "/assets/Nn_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterO').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_15").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Oo_LP.mp3",
+					oga: "/assets/Oo_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterP').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_16").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Pp_LP.mp3",
+					oga: "/assets/Pp_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterQ').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_17").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Qq_LP.mp3",
+					oga: "/assets/Qq_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterR').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_18").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Rr_LP.mp3",
+					oga: "/assets/Rr_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterS').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_19").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Ss_LP.mp3",
+					oga: "/assets/Ss_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterT').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_20").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Tt_LP.mp3",
+					oga: "/assets/Tt_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterU').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_21").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Uu_LP.mp3",
+					oga: "/assets/Uu_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterV').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_22").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Vv_LP.mp3",
+					oga: "/assets/Vv_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterW').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_23").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Ww_LP.mp3",
+					oga: "/assets/Ww_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterX').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_24").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Xx_LP.mp3",
+					oga: "/assets/Xx_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterY').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_25").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Yy_LP.mp3",
+					oga: "/assets/Yy_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+	$('#letterZ').click(function(){
+		$(".jp-jplayer").jPlayer("destroy");
+		$("#jquery_jplayer_26").jPlayer({
+			ready: function (event) {
+				$(this).jPlayer("setMedia", {
+					m4a: "/assets/Zz_LP.mp3",
+					oga: "/assets/Zz_LP.ogg"
+				}).jPlayer("play");
+			},
+			supplied: "m4a, oga",
+			wmode: "window"
+		});
+	});	
+});
