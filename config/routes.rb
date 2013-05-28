6538643567b035f5a8c0f1b210754a1968c723b4
Kevin <diasks2@gmail.com>
2013-05-28 22:50:56 +0900

    Add phonics only

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Phonics::Application.routes.draw do
    root to: 'static_pages#home'
    match '/touch', to: 'static_pages#touch'
    match '/sound', to: 'static_pages#sound'
+   match '/phonics', to: 'static_pages#phonics'
 end
